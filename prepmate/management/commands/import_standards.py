827e39088c3855dafc828d931d797c38add8eaa5
Paige Mason <paigecantz@Paiges-MacBook-Pro-3.local>
2026-03-05 14:20:36 -0500

    Fix import_standards Command

--- prepmate/management/commands/import_standards.py
+++ prepmate/management/commands/import_standards.py
@@ -1,31 +1,37 @@
 import csv
 import os
 from django.conf import settings
+from django.core.management.base import BaseCommand
 from prepmate.models import Standard
 
+class Command(BaseCommand):
+     help = "Import standards from csv files"
 
-csv_files = {
-       "math": "ccss_math.csv",
-       "ela": "ccss_ela.csv"
-}
+     def handle(self, *args, **kwargs):
+          
+        csv_files = {
+            "math": "ccss_math.csv",
+            "ela": "ccss_ela.csv"
+        }
 
-for subject, filename in csv_files.items():
-    file_path = os.path.join(settings.BASE_DIR, 'prepmate', 'data', filename)
+        for subject, filename in csv_files.items():
+            file_path = os.path.join(settings.BASE_DIR, 'prepmate', 'data', filename)
 
-    with open(file_path, newline="", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
+            with open(file_path, newline="", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
 
-            # Create new standard object for each row
-                Standard.objects.get_or_create(
-                    code=row["id"].strip(),
-                    defaults={
-                        "content_type": row["content_type"].strip(),
-                        "category_id": row["category_id"].strip(),
-                        "category_name": row["category_name"].strip(),
-                        "grade_id": row["grade_id"].strip(),
-                        "grade_name": row["grade_name"].strip(),
-                        "item": row["item"].strip(),
-                        "description": row["description"].strip()
-                    }
-            )
+                    # Create new standard object for each row
+                        Standard.objects.get_or_create(
+                            code=row["id"].strip(),
+                            defaults={
+                                "content_type": row["content_type"].strip(),
+                                "category_id": row["category_id"].strip(),
+                                "category_name": row["category_name"].strip(),
+                                "grade_id": row["grade_id"].strip(),
+                                "grade_name": row["grade_name"].strip(),
+                                "item": row["item"].strip(),
+                                "description": row["description"].strip()
+                            }
+                    )
+        self.stdout.write(self.style.SUCCESS("Standards successfully imported."))
